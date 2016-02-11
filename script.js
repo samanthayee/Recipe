135903f3b255cf5f0a7118177b62ef1adb8aa2e7
samanthayee <samanthayee@Samanthas-MBP.phub.net.cable.rogers.com>
2016-02-10 19:22:27 -0500

    Banana Bread Recipe

---
+++ script.js
@@ -0,0 +1,113 @@
+$(document).ready(function() {
+	$('.ingredients ul li:first-child').click(function() {
+		$(this).css('text-decoration', 'line-through');
+		
+			$('.ingredients ul li:first-child').click(function() {
+				$(this).css('text-decoration', 'none');
+			});
+	});
+
+	$('.ingredients ul li:nth-child(2)').click(function() {
+		$(this).css('text-decoration', 'line-through');
+		
+			$('.ingredients ul li:nth-child(2)').click(function() {
+				$(this).css('text-decoration', 'none');
+			});
+	});
+
+	$('.ingredients ul li:nth-child(3)').click(function() {
+		$(this).css('text-decoration', 'line-through');
+		
+			$('.ingredients ul li:nth-child(3)').click(function() {
+				$(this).css('text-decoration', 'none');
+			});
+	});
+
+	$('.ingredients ul li:nth-child(4)').click(function() {
+		$(this).css('text-decoration', 'line-through');
+		
+			$('.ingredients ul li:nth-child(4)').click(function() {
+				$(this).css('text-decoration', 'none');
+			});
+	});
+
+	$('.ingredients ul li:nth-child(5)').click(function() {
+		$(this).css('text-decoration', 'line-through');
+		
+			$('.ingredients ul li:nth-child(5)').click(function() {
+				$(this).css('text-decoration', 'none');
+			});
+	});
+
+	$('.ingredients ul li:nth-child(6)').click(function() {
+		$(this).css('text-decoration', 'line-through');
+		
+			$('.ingredients ul li:nth-child(6)').click(function() {
+				$(this).css('text-decoration', 'none');
+			});
+	});
+
+	$('.ingredients ul li:nth-child(7)').click(function() {
+		$(this).css('text-decoration', 'line-through');
+		
+			$('.ingredients ul li:nth-child(7)').click(function() {
+				$(this).css('text-decoration', 'none');
+			});
+	});	
+
+	$('.ingredients ul li:nth-child(8)').click(function() {
+		$(this).css('text-decoration', 'line-through');
+		
+			$('.ingredients ul li:nth-child(8)').click(function() {
+				$(this).css('text-decoration', 'none');
+			});
+	});
+
+	$('.ingredients ul li:nth-child(9)').click(function() {
+		$(this).css('text-decoration', 'line-through');
+		
+			$('.ingredients ul li:nth-child(9)').click(function() {
+				$(this).css('text-decoration', 'none');
+			});
+	});
+
+	$('.ingredients ul li:nth-child(10)').click(function() {
+		$(this).css('text-decoration', 'line-through');
+		
+			$('.ingredients ul li:nth-child(10)').click(function() {
+				$(this).css('text-decoration', 'none');
+			});
+	});	
+
+
+	$('.directions ol li:nth-child(2)').click(function() {
+		$(this).css('color', '#e0e0eb');
+	});	
+
+	$('.directions ol li:first-child').click(function() {
+		$(this).css('color', '#e0e0eb');
+	});	
+		$('.directions ol li:nth-child(2)').click(function() {
+		$(this).css('color', '#e0e0eb');
+	});	
+		$('.directions ol li:nth-child(3)').click(function() {
+		$(this).css('color', '#e0e0eb');
+	});	
+		$('.directions ol li:nth-child(4)').click(function() {
+		$(this).css('color', '#e0e0eb');
+	});	
+		$('.directions ol li:nth-child(5)').click(function() {
+		$(this).css('color', '#e0e0eb');
+	});	
+		$('.directions ol li:nth-child(6)').click(function() {
+		$(this).css('color', '#e0e0eb');
+	});	
+		$('.directions ol li:nth-child(7)').click(function() {
+		$(this).css('color', '#e0e0eb');
+	});	
+		$('.directions ol li:nth-child(8)').click(function() {
+		$(this).css('color', '#e0e0eb');
+	});	
+
+
+});
